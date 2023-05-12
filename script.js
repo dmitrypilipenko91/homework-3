632a76269cd20cc74f97860c513b25d9c07d909a
dmitrypilipenko91 <dmitrypilipenko91@gmail.com>
2023-05-12 05:06:30 +0500

    Corrected mistakes in script.js

--- script.js
+++ script.js
@@ -1,52 +1,51 @@
 function HashStorageFunc( key,value ) {
-    const self = this;
-    const privStorage = {};
-    self.addValue = function( key,value ) {
-      privStorage.key = value;
+  const self = this;
+  const privStorage = {};
+  self.addValue = function( key,value ) {
+    privStorage[key] = value;
+  }
+  self.getValue = function( key ) {
+    return privStorage[key];
+  }
+  self.deleteValue = function( key ) {
+    if ( key in privStorage ) {
+      delete privStorage[key];
+      return true;
     }
-    self.getValue = function( key ) {
-      return privStorage.key;
-    }
-    self.deleteValue = function( key ) {
-      if ( key in privStorage ) {
-        delete privStorage[key];
-        return true;
-      }
-      return false;
-    }
-    self.getKeys = function() {
-      return Object.keys( privStorage );
+    return false;
+  }
+  self.getKeys = function() {
+    return Object.keys( privStorage );
+  }
+}
+
+const drinkStorage = new HashStorageFunc ( );
+
+function addInfo() {
+  let drinkName = prompt( "Введите название напитка" );
+  let drinkAlc = prompt( "Алкогольный" );
+  let drinkRecipe = prompt( "Рецепт" );
+  drinkStorage.addValue ( drinkName, { "Алкогольный": drinkAlc, "Рецепт": drinkRecipe } );
+  alert( "Информация о напитке добавлена!" );
+}
+function getInfo() {
+  let drinkName = prompt( "Введите название напитка" );
+  let drinkInfo = drinkStorage.getValue( drinkName );
+  if (drinkInfo) {
+    for (let k in drinkInfo) {
+      alert( "Напиток: " + drinkName + "\n"
+                + k + ": " + drinkInfo[k] );
     }
   }
-  
-  const drinkStorage = new HashStorageFunc ( );
-  
-  function addInfo() {
-    let userDrinkName = prompt( "Введите название напитка" );
-    let userDrinkAlc = prompt( "Алкогольный" );
-    let userDrinkRecipe = prompt( "Рецепт" );
-    drinkStorage.addValue ( userDrinkName, { "Алкогольный": userDrinkAlc, "Рецепт": userDrinkRecipe } );
-    alert( "Информация о напитке добавлена!" );
-  }
-  function getInfo() {
-    let userDrinkName = prompt( "Введите название напитка" );
-    let drinkInfo = drinkStorage.getValue( userDrinkName );
-    if (drinkInfo) {
-      for (let k in drinkInfo) {
-        alert( "Напиток: " + userDrinkName + "\n"
-                  + k + ": " + drinkInfo[k] );
-      }
-    }
-    else alert( 'Нет информации о напитке' );
-  }
-  function deleteInfo() {
-    let userDrinkName = prompt( "Введите название напитка" );
-    let result = drinkStorage.deleteValue( userDrinkName );
-    if (result) 
-      alert( "Информация удалена!" );
-    else alert( "Напиток не найден!" );
-  }
-  function getList() {
-    alert( drinkStorage.getKeys() );
-  }
-  
+  else alert( 'Нет информации о напитке' );
+}
+function deleteInfo() {
+  let drinkName = prompt( "Введите название напитка" );
+  let result = drinkStorage.deleteValue( drinkName );
+  if (result) 
+    alert( "Информация удалена!" );
+  else alert( "Напиток не найден!" );
+}
+function getList() {
+  alert( drinkStorage.getKeys() );
+}
